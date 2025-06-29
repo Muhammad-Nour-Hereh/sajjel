ce7e446351c497c90194e05c83d319ad7c5d10b5
mnh <acadamiclearner@gmail.com>
2025-06-29 11:07:52 +0300

    create login/register routes'

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -2,12 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import { UserRoutes } from "./routes/UserRoutes";
 import HomePage from "./pages/HomePage";
 import UserLayout from "./layouts/UserLayout";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<UserLayout />}>
         <Route path={UserRoutes.HOME} element={<HomePage />} />
+        <Route path={UserRoutes.REGISTER} element={<RegisterPage />} />
+        <Route path={UserRoutes.LOGIN} element={<LoginPage/>} />
       </Route>
     </Routes>
   );
