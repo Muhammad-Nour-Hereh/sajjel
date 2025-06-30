ad37ad0cdf435b397d56e91880bbcf5c5ae14f25
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-06-30 09:24:32 +0300

    add logout button

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from 'react'
 import useMainPage from '../hooks/useMainPage'
 import { remote } from '../remotes/remotes'
+import { UserRoutes } from '@/routes/UserRoutes'
+import { useNavigate } from 'react-router-dom'
 
 const HomePage = () => {
   const { title } = useMainPage()
+  const navigate = useNavigate()
   const call = async () => {
     const res = await remote.auth.login('test@example.com', 'password')
     console.log(res)
@@ -11,7 +14,20 @@ const HomePage = () => {
   useEffect(() => {
     call()
   }, [])
-  return <div className="p-10">{title}</div>
+  return (
+    <div className="p-10">
+      {title}{' '}
+      <a
+        onClick={() => {
+          remote.auth.logout()
+          localStorage.removeItem('access_token')
+          navigate(UserRoutes.LOGIN)
+        }}
+        className="block cursor-pointer py-2 text-sm select-none hover:text-gray-700">
+        Logout
+      </a>
+    </div>
+  )
 }
 
 export default HomePage
