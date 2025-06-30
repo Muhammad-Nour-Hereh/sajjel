fccdbfb9b6d36c5d07bf95b32d3fd10e9b9323a1
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-06-30 09:35:52 +0300

    don't forget it next tiem :)

---
+++ client/src/pages/ForgetPasswordPage.tsx
@@ -0,0 +1,9 @@
+const ForgetPasswordPage = () => {
+  return (
+    <h1 className="flex items-center justify-center p-50 text-3xl font-extrabold">
+      don't forget it next time 🙃
+    </h1>
+  )
+}
+
+export default ForgetPasswordPage
