82d1f536775dae72f28e5987d35822adef55fdb1
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-07-05 22:12:52 +0300

    make server returns valid thumbnails

--- server/database/factories/ItemFactory.php
+++ server/database/factories/ItemFactory.php
@@ -6,13 +6,15 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ItemFactory extends Factory {
     public function definition(): array {
+        $placeholderUrl = config('app.url') . '/thumbnails/item_thumbnail_placeholder.png';
+
         return [
-            'name'        => $this->faker->word(),
-            'model'       => $this->faker->bothify('Model-###??'),
-            'buy_price'   => $this->faker->randomFloat(2, 10, 500),
-            'sell_price'  => $this->faker->randomFloat(2, 20, 1000),
-            'thumbnail'   => $this->faker->imageUrl(640, 480, 'technics', true),
-            'note'        => $this->faker->sentence(),
+            'name'       => $this->faker->word(),
+            'model'      => $this->faker->bothify('Model-###??'),
+            'buy_price'  => $this->faker->randomFloat(2, 10, 500),
+            'sell_price' => $this->faker->randomFloat(2, 20, 1000),
+            'thumbnail'  => $placeholderUrl,
+            'note'       => $this->faker->sentence(),
         ];
     }
 }
