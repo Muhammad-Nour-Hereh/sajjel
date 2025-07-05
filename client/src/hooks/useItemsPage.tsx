296e195c234df8c817ba8a1110d9aaf7535d96de
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-07-05 21:35:26 +0300

    display items in cards

--- client/src/hooks/useItemsPage.tsx
+++ client/src/hooks/useItemsPage.tsx
@@ -1,5 +1,21 @@
+import { remote } from '@/remotes/remotes'
+import { useEffect, useState } from 'react'
+import type { Item } from '@/models/Item'
+
 const useItemsPage = () => {
-  const items = [{ title: 'a' }, { title: 'a' }, { title: 'a' }, { title: 'a' }]
+  const [items, setItems] = useState<Item[]>([])
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await remote.items.fetchAll()
+      console.log(result.data)
+      if (result.data === undefined) result.data = []
+      setItems(result.data)
+    }
+
+    fetchItems()
+  }, [])
+
   return { items }
 }
 
--- client/src/pages/ItemsPage.tsx
+++ client/src/pages/ItemsPage.tsx
@@ -1,12 +1,22 @@
 import useItemsPage from '@/hooks/useItemsPage'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 const ItemsPage = () => {
   const { items } = useItemsPage()
 
   return (
-    <div>
-      {items.map((item, index) => (
-        <p key={index}>{item.title}</p>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      {items.map((item) => (
+        <Card key={item.id} className="hover:shadow-md transition">
+          <CardHeader>
+            <CardTitle>{item.name}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Model: {item.model || 'N/A'}</p>
+            <p>Buy: {item.buy_price?.toString()}</p>
+            <p>Sell: {item.sell_price?.toString()}</p>
+          </CardContent>
+        </Card>
       ))}
     </div>
   )
