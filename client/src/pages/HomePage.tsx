8bb606c2ede369c1987d99f5d0e962a2271bd4dc
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-07-09 13:41:10 +0300

    styling home page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import useMainPage from '../hooks/useMainPage'
 import { remote } from '../remotes/remotes'
 import { useNavigate } from 'react-router-dom'
 import { GuestRoutes } from '@/routes/GuestRoutes'
+import { UserRoutes } from '@/routes/UserRoutes'
 
 const HomePage = () => {
   const { title } = useMainPage()
@@ -16,16 +17,25 @@ const HomePage = () => {
   }, [])
   return (
     <div className="p-10">
-      {title}{' '}
-      <a
-        onClick={() => {
-          remote.auth.logout()
-          localStorage.removeItem('access_token')
-          navigate(GuestRoutes.LOGIN)
-        }}
-        className="block cursor-pointer py-2 text-sm select-none hover:text-gray-700">
-        Logout
-      </a>
+      <h1 className="font-bold text-3xl">{title}</h1>
+      <ul>
+        <li
+          onClick={() => {
+            navigate(UserRoutes.ITEMS)
+          }}
+          className="block cursor-pointer py-2 text-sm select-none hover:text-gray-700">
+          Items
+        </li>
+        <li
+          onClick={() => {
+            remote.auth.logout()
+            localStorage.removeItem('access_token')
+            navigate(GuestRoutes.LOGIN)
+          }}
+          className="block cursor-pointer py-2 text-sm select-none hover:text-gray-700">
+          Logout
+        </li>
+      </ul>
     </div>
   )
 }
