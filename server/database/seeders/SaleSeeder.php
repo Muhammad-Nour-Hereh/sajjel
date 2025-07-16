f4a2e48481fe587fca8d5dde5b1cb13730e9fa34
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-07-16 15:28:09 +0300

    create SaleSeeder

--- server/database/seeders/SaleSeeder.php
+++ server/database/seeders/SaleSeeder.php
@@ -2,16 +2,14 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Sale;
 use Illuminate\Database\Seeder;
 
 class SaleSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
     {
-        //
+        Sale::factory()->count(100)->create();
     }
 }
