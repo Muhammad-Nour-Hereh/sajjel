e7da5f839a58f5f5e313a21983bec5444963f67e
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-08-12 19:10:41 +0300

    add pen icon

--- client/src/pages/SalesPage.tsx
+++ client/src/pages/SalesPage.tsx
@@ -26,7 +26,7 @@ import {
   TableRow,
 } from '@/components/ui/table'
 import useSalesPage from '@/hooks/useSalesPage'
-import { Calendar, Filter, Trash2 } from 'lucide-react'
+import { Calendar, Filter, Pen, Trash2 } from 'lucide-react'
 
 const dateFilterOptions = [
   { value: 'today', label: 'Today' },
@@ -147,16 +147,26 @@ const SalesPage = () => {
                     </Badge>
                   ))}
                 </div>
-                <button
-                  onClick={(e) => {
-                    e.stopPropagation()
-                    setSaleToDelete(sale.id)
-                    setConfirmOpen(true)
-                  }}
-                  aria-label="Delete sale"
-                  className="ml-4 text-red-500 hover:text-red-700 active:text-red-900 transition-colors duration-150">
-                  <Trash2 />
-                </button>
+                <div>
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation()
+                    }}
+                    aria-label="Delete sale"
+                    className="ml-4 text-gray-500 hover:text-gray-700 active:text-gray-900 transition-colors duration-150">
+                    <Pen />
+                  </button>
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation()
+                      setSaleToDelete(sale.id)
+                      setConfirmOpen(true)
+                    }}
+                    aria-label="Delete sale"
+                    className="ml-4 text-red-500 hover:text-red-700 active:text-red-900 transition-colors duration-150">
+                    <Trash2 />
+                  </button>
+                </div>
               </div>
             </AccordionTrigger>
             <AccordionContent className="bg-muted px-4 pb-4">
