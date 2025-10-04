47db7802a4267c7ccc0bed182c7ad665a04fe78c
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-10-04 14:22:48 +0300

    extract Icon from StorageCard

--- client/src/features/inventory/components/StorageCard.tsx
+++ client/src/features/inventory/components/StorageCard.tsx
@@ -27,61 +27,59 @@ interface StorageCardProps {
   setConfirmDialog: (val: boolean) => void
 }
 
+const Icon = ({ storageType }: { storageType: StorageType }) => {
+  const Icon = storageTypeIcons[storageType].icon
+  const color = storageTypeIcons[storageType].color
+  return (
+    <div className={`p-2 rounded-lg ${color}`}>
+      <Icon className={`h-5 w-5`} />
+    </div>
+  )
+}
+
 const StorageCard = ({
   name,
   storageType,
   openDialog,
   setFormData,
   setConfirmDialog,
-}: StorageCardProps) => {
-  const Icon = () => {
-    const Icon = storageTypeIcons[storageType].icon
-    const color = storageTypeIcons[storageType].color
-    return (
-      <div className={`p-2 rounded-lg ${color}`}>
-        <Icon className={`h-5 w-5`} />
-      </div>
-    )
-  }
-
-  return (
-    <Card className="px-4">
-      <div className="flex items-center gap-4">
-        <Icon />
+}: StorageCardProps) => (
+  <Card className="px-4">
+    <div className="flex items-center gap-4">
+      <Icon storageType={storageType} />
+      <div>
+        <span className="text-lg font-bold">{name}</span>
         <div>
-          <span className="text-lg font-bold">{name}</span>
-          <div>
-            <Badge variant="secondary" className="mt-1 capitalize">
-              {storageType}
-            </Badge>
-          </div>
+          <Badge variant="secondary" className="mt-1 capitalize">
+            {storageType}
+          </Badge>
         </div>
       </div>
-      <div className="flex gap-2">
-        <Button
-          className="flex-auto"
-          variant={'outline'}
-          size="sm"
-          onClick={() => {
-            setFormData({
-              name: name,
-              storageType: storageType,
-            })
-            openDialog()
-          }}>
-          <Edit /> Edit
-        </Button>
-        <Button
-          variant={'destructive'}
-          size="sm"
-          onClick={() => {
-            setConfirmDialog(true)
-          }}>
-          <Trash2 />
-        </Button>
-      </div>
-    </Card>
-  )
-}
+    </div>
+    <div className="flex gap-2">
+      <Button
+        className="flex-auto"
+        variant={'outline'}
+        size="sm"
+        onClick={() => {
+          setFormData({
+            name: name,
+            storageType: storageType,
+          })
+          openDialog()
+        }}>
+        <Edit /> Edit
+      </Button>
+      <Button
+        variant={'destructive'}
+        size="sm"
+        onClick={() => {
+          setConfirmDialog(true)
+        }}>
+        <Trash2 />
+      </Button>
+    </div>
+  </Card>
+)
 
 export default StorageCard
